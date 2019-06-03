080d3be807941e461aa074a14d2c0387d4df506d
asuarezv <36944032+asuarezv@users.noreply.github.com>
2019-06-02 23:18:58 -0500

    Efecto
    Efecto de presión y liberación de botones

---
+++ js/app.js
@@ -0,0 +1,80 @@
+function presionarBotonOn()       {document.getElementById('on').style="padding: 1px 1px;";}
+function presionarBotonSign()     {document.getElementById('sign').style="padding: 1px 1px;";}
+function presionarBotonRaiz()     {document.getElementById('raiz').style="padding: 1px 1px;";}
+function presionarBotonDividido() {document.getElementById('dividido').style="padding: 1px 1px;";}
+function presionarBoton7()        {document.getElementById('7').style="padding: 1px 1px;";}
+function presionarBoton8()        {document.getElementById('8').style="padding: 1px 1px;";}
+function presionarBoton9()        {document.getElementById('9').style="padding: 1px 1px;";}
+function presionarBotonPor()      {document.getElementById('por').style="padding: 1px 1px;";}
+function presionarBoton4()        {document.getElementById('4').style="padding: 1px 1px;";}
+function presionarBoton5()        {document.getElementById('5').style="padding: 1px 1px;";}
+function presionarBoton6()        {document.getElementById('6').style="padding: 1px 1px;";}
+function presionarBotonMenos()    {document.getElementById('menos').style="padding: 1px 1px;";}
+function presionarBoton1()        {document.getElementById('1').style="padding: 1px 1px;";}
+function presionarBoton2()        {document.getElementById('2').style="padding: 1px 1px;";}
+function presionarBoton3()        {document.getElementById('3').style="padding: 1px 1px;";}
+function presionarBoton0()        {document.getElementById('0').style="padding: 1px 1px;";}
+function presionarBotonPunto()    {document.getElementById('punto').style="padding: 1px 1px;";}
+function presionarBotonIgual()    {document.getElementById('igual').style="padding: 1px 1px;";}
+function presionarBotonMas()      {document.getElementById('mas').style="padding: 1px 1px 0px 0px;";}
+
+function liberarBotonOn()         {document.getElementById('on').style="padding: 0px 0px;";}
+function liberarBotonSign()       {document.getElementById('sign').style="padding: 0px 0px;";}
+function liberarBotonRaiz()       {document.getElementById('raiz').style="padding: 0px 0px;";}
+function liberarBotonDividido()   {document.getElementById('dividido').style="padding: 0px 0px;";}
+function liberarBoton7()          {document.getElementById('7').style="padding: 0px 0px;";}
+function liberarBoton8()          {document.getElementById('8').style="padding: 0px 0px;";}
+function liberarBoton9()          {document.getElementById('9').style="padding: 0px 0px;";}
+function liberarBotonPor()        {document.getElementById('por').style="padding: 0px 0px;";}
+function liberarBoton4()          {document.getElementById('4').style="padding: 0px 0px;";}
+function liberarBoton5()          {document.getElementById('5').style="padding: 0px 0px;";}
+function liberarBoton6()          {document.getElementById('6').style="padding: 0px 0px;";}
+function liberarBotonMenos()      {document.getElementById('menos').style="padding: 0px 0px;";}
+function liberarBoton1()          {document.getElementById('1').style="padding: 0px 0px;";}
+function liberarBoton2()          {document.getElementById('2').style="padding: 0px 0px;";}
+function liberarBoton3()          {document.getElementById('3').style="padding: 0px 0px;";}
+function liberarBoton0()          {document.getElementById('0').style="padding: 0px 0px;";}
+function liberarBotonPunto()      {document.getElementById('punto').style="padding: 0px 0px;";}
+function liberarBotonIgual()      {document.getElementById('igual').style="padding: 0px 0px;";}
+function liberarBotonMas()        {document.getElementById('mas').style="padding: 0px 0px;";}
+
+
+document.getElementById("on").addEventListener("mousedown", presionarBotonOn);
+document.getElementById("sign").addEventListener("mousedown", presionarBotonSign);
+document.getElementById("raiz").addEventListener("mousedown", presionarBotonRaiz);
+document.getElementById("dividido").addEventListener("mousedown", presionarBotonDividido);
+document.getElementById("7").addEventListener("mousedown", presionarBoton7);
+document.getElementById("8").addEventListener("mousedown", presionarBoton8);
+document.getElementById("9").addEventListener("mousedown", presionarBoton9);
+document.getElementById("por").addEventListener("mousedown", presionarBotonPor);
+document.getElementById("4").addEventListener("mousedown", presionarBoton4);
+document.getElementById("5").addEventListener("mousedown", presionarBoton5);
+document.getElementById("6").addEventListener("mousedown", presionarBoton6);
+document.getElementById("menos").addEventListener("mousedown", presionarBotonMenos);
+document.getElementById("1").addEventListener("mousedown", presionarBoton1);
+document.getElementById("2").addEventListener("mousedown", presionarBoton2);
+document.getElementById("3").addEventListener("mousedown", presionarBoton3);
+document.getElementById("0").addEventListener("mousedown", presionarBoton0);
+document.getElementById("punto").addEventListener("mousedown", presionarBotonPunto);
+document.getElementById("igual").addEventListener("mousedown", presionarBotonIgual);
+document.getElementById("mas").addEventListener("mousedown", presionarBotonMas);
+
+document.getElementById("on").addEventListener("mouseup", liberarBotonOn);
+document.getElementById("sign").addEventListener("mouseup", liberarBotonSign);
+document.getElementById("raiz").addEventListener("mouseup", liberarBotonRaiz);
+document.getElementById("dividido").addEventListener("mouseup", liberarBotonDividido);
+document.getElementById("7").addEventListener("mouseup", liberarBoton7);
+document.getElementById("8").addEventListener("mouseup", liberarBoton8);
+document.getElementById("9").addEventListener("mouseup", liberarBoton9);
+document.getElementById("por").addEventListener("mouseup", liberarBotonPor);
+document.getElementById("4").addEventListener("mouseup", liberarBoton4);
+document.getElementById("5").addEventListener("mouseup", liberarBoton5);
+document.getElementById("6").addEventListener("mouseup", liberarBoton6);
+document.getElementById("menos").addEventListener("mouseup", liberarBotonMenos);
+document.getElementById("1").addEventListener("mouseup", liberarBoton1);
+document.getElementById("2").addEventListener("mouseup", liberarBoton2);
+document.getElementById("3").addEventListener("mouseup", liberarBoton3);
+document.getElementById("0").addEventListener("mouseup", liberarBoton0);
+document.getElementById("punto").addEventListener("mouseup", liberarBotonPunto);
+document.getElementById("igual").addEventListener("mouseup", liberarBotonIgual);
+document.getElementById("mas").addEventListener("mouseup", liberarBotonMas);
